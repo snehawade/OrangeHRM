67e0f4e08373e5835b57f730097cda637f18e97e
HP <snehawade123@gmail.com>
2024-06-10 18:02:25 +0530

    Test case: valid and invalid id and password

--- src/test/java/com/tests/LoginPageTest.java
+++ src/test/java/com/tests/LoginPageTest.java
@@ -5,30 +5,42 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.baseclass.Baseclass;
+import com.pomclasses.LoginPagePom;
 
 public class LoginPageTest extends Baseclass {
-	
+	LoginPagePom loginpagepom;
+
 	@BeforeClass
 	public void setup() {
-		getWebDriver();
+		// getWebDriver();
+		loginpagepom = new LoginPagePom();
 	}
-		
+
 	@AfterClass
 	public void teardown() {
-		driver.close();
+		// driver.close();
 	}
-	
+
 	@Test
 	public void pageUrl() {
-		
-		String pageurl = getWebDriver().getCurrentUrl();
-		
-		if(pageurl.equalsIgnoreCase("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")) {
+
+		String pageurl = driver.getCurrentUrl();
+
+		if (pageurl.equalsIgnoreCase("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")) {
 			System.out.println("We are on correct page....Test pass");
 		}
-		
-		
+
 	}
-	
+
+	@Test
+	public void loginTest() {
+		loginpagepom.login(loginpagepom.getusername(), loginpagepom.getpassword());
+	}
+
+	@Test
+	public void invalidloginTest() {
+		loginpagepom.login(loginpagepom.getusername(), "123456");
+		System.out.println(loginpagepom.getinvalidloginerrormsg());
+	}
 
 }
